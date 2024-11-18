a4fecdc32575e7649e988a1935e4d302e17b7101
Bajraktar zi Lvovu <39407654+looomenn@users.noreply.github.com>
2024-11-19 00:07:59 +0200

    refactor: method of exporting text styles

--- letterguesser/styles/font.py
+++ letterguesser/styles/font.py
@@ -5,8 +5,23 @@ Defines font types, sizes, and line heights used across the application,
 ensuring consistency and readability in the UI.
 """
 
+font_base: int = 16
+
+fonts: dict = {
+    'fontStack-system': "'Segoe UI', 'Noto Sans', 'Helvetica', 'Arial', 'sans-serif'",
+    'text-title-weight-large': 600,
+    'text-title-weight-medium': 600,
+    'text-title-weight-small': 600,
+    'text-title-size-large': f'{font_base * 2}px',
+    'text-title-size-medium': f'{font_base * 2}px',
+    'text-title-size-small': f'{font_base * 1}px',
+    'text-body-size-large': f"{font_base * 1}px",
+    'text-body-size-medium': f"{font_base * 0.875}px",
+    'text-body-size-small': f"{font_base * 0.75}px"
+}
+
 # general settings
-font: str = 'Inter'
+font: str = "Geist Mono"
 text_large: int = 18
 text_medium: int = 16
 text_small: int = 14
