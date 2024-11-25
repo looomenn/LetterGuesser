1264063265087ea90cdab0ef04e2ca72046b4044
Bajraktar zi Lvovu <39407654+looomenn@users.noreply.github.com>
2024-11-25 19:28:00 +0200

    refactor: migrate TextBlockSegment to PyQt6

--- letterguesser/gui/widgets/TextBlockSegment.py
+++ letterguesser/gui/widgets/TextBlockSegment.py
@@ -6,14 +6,15 @@ This widget shows a labeled text block with localization and updates support.
 
 from typing import Any
 
-import customtkinter as ctk
+from PyQt6.QtWidgets import QVBoxLayout, QLabel, QLineEdit, QWidget, QGridLayout
+from PyQt6.QtCore import Qt
 
-from letterguesser.gui.frames.BaseFrame import BaseFrame
-from letterguesser.styles.font import font, text_small, text_small_height
 from letterguesser.styles.padding import pad_0, pad_1
 
+from letterguesser.context import localisation, manager
 
-class TextBlockSegment(BaseFrame):
+
+class TextBlockSegment(QWidget):
     """A text block widget with localization and automatic updating."""
 
     def __init__(
@@ -31,44 +32,32 @@ class TextBlockSegment(BaseFrame):
         :param initial_text: Initial text to display in the text block.
         :param kwargs: Additional configuration options.
         """
-        super().__init__(parent, transparent_bg=True, **kwargs)
+        super().__init__(parent, **kwargs)
 
-        self.text_var = ctk.StringVar(value=initial_text)
+        self.localisation = localisation
+        self.manager = manager
 
-        self.label = ctk.CTkLabel(
-            self,
-            height=text_small_height,
-            anchor='w',
-            font=ctk.CTkFont(
-                family=font,
-                size=text_small
-            )
+        # layout
+        self.layout = QGridLayout(self)
+        self.layout.setContentsMargins(pad_0, pad_0, pad_0, pad_0)
+        self.layout.setSpacing(pad_0)
+
+        # label
+        self.label = QLabel()
+        self.label.setAlignment(
+            Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignVCenter
         )
+
+        # text block
+        self.text_block = QLineEdit()
+        self.text_block.setText(initial_text)
+        self.setEnabled(False)
+
         self.localisation.bind(self.label, localisation_key)
-        self.add_widget(
-            self.label,
-            side='top',
-            pady=(pad_0, pad_1)
-        )
-
-        self.text_block = ctk.CTkEntry(
-            self,
-            textvariable=self.text_var,
-            state='disabled',
-            height=40,
-            border_width=0
-        )
         self.localisation.bind(self.text_block, 'no_input', self.update_value)
 
-        self.add_widget(
-            self.text_block,
-            side='bottom',
-            pady=(pad_1, pad_0)
-        )
-
-        self.text_block.configure(
-            text_color=ctk.ThemeManager.theme['CTkEntry']['placeholder_text_color'][0]
-            )
+        self.layout.addWidget(self.label)
+        self.layout.addWidget(self.text_block)
 
     def rebind(self, new_loc_key: str) -> None:
         """
@@ -84,7 +73,7 @@ class TextBlockSegment(BaseFrame):
 
         :param new_value: New text to display in the text block.
         """
-        self.text_var.set(new_value)
+        self.text_block.setText(new_value)
 
     def reset(self) -> None:
         """Reset the text block to the initial localization key."""
